33e3da9bc3ddbf0505573656e1f419b2b2405a59
liaowanghui <liaowanghui@chinamobile.com>
2020-08-12 17:46:01 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/mapper/CourseMapper.java
@@ -0,0 +1,11 @@
+package com.example.demo.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.demo.entity.Course;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseMapper extends BaseMapper<Course> {
+
+}
---
+++ src/main/java/com/example/demo/mapper/UdictMapper.java
@@ -0,0 +1,10 @@
+package com.example.demo.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.demo.entity.Udict;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UdictMapper extends BaseMapper<Udict> {
+}
